3dfd1bac8c2a0b7b3de29acd4a8791d9f73a91b4
mateussilvasouza <mateus.souza@vaifacil.com>
2023-11-14 12:26:38 -0300

    Fix-env_import

---
+++ src/env/index.ts
@@ -0,0 +1,28 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  DB_CLIENT: z.enum(['sqlite', 'pg']),
+  DB_URL: z.string(),
+  DB_PORT: z.coerce.number().default(8888),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.log(_env)
+  console.error('Invalid enviroment variables!', _env.error.format())
+
+  throw new Error('Invalid enviroment variables.')
+}
+
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { env } from './.env'
+import { env } from './env'
 import app from './app'
 
 app.listen({ port: env.DB_PORT }).then(() => console.log('HTTP Server Running'))
