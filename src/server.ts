2efeefa9ac29640b6ffae47a73f6de0a8eba2ce5
mateussilvasouza <mateus.souza@vaifacil.com>
2023-11-14 12:50:00 -0300

    Alter-listen-for-server.js

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import app from './app'
 
 app
   .listen({
-    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
     port: env.PORT,
   })
   .then(() => console.log('HTTP Server Running'))
